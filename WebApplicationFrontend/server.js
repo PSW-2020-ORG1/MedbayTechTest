a40d5637f729dbf2a8d4dc68a6ef916fd253aba9
jelena-vlajkov <vlajkovj31@gmail.com>
2020-12-13 10:22:18 +0100

    test

--- WebApplicationFrontend/server.js
+++ WebApplicationFrontend/server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/dist/WebApplicationFrontend'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/WebApplicationFrontend/index.html'));   
 });  
 
 server.listen(port, function() {
