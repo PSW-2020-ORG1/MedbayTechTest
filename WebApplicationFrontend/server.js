ccde342513aa00053bad1e910f760d9ca588b19d
jelena-vlajkov <vlajkovj31@gmail.com>
2020-12-13 07:47:41 +0100

    test

--- WebApplicationFrontend/server.js
+++ WebApplicationFrontend/server.js
@@ -12,5 +12,9 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/WebApplicationFrontend/index.html'));   
 });  
 
+server.listen(port, function() {
+    console.log("App running on port " + port);
+});
+
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
